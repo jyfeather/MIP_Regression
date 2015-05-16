00f02db89a6bd59b9be0771fc866cae4b6c280f0
Yan Jin <yanjin@ougl-72-g.eplt.washington.edu>
2015-05-15 17:03:19 -0700

    implement matrix inversion

--- example/example2.R
+++ example/example2.R
@@ -8,7 +8,7 @@
 rm(list=ls())
 
 library("Rglpk")
-library("linprog")
+library("lpSolveAPI")
 
 source("./R/PredictionVector.R")
 source("./R/BAB.R")
@@ -19,48 +19,45 @@ source("./R/PredictionModel.R")
 #  input
 ####################################
 path <- paste(getwd(), "/example/Data/Sample/air05", sep = "")
-model <- Rglpk_read_file(path, type = "MPS_fixed")
+model <- read.lp(filename = path, type = "mps")
+ncol <- length(get.type(model))
+set.bounds(model, upper = rep(Inf, ncol), columns = 1:ncol) # remove upper bound, currently limited
+set.type(model, columns = 1:ncol, type = "real") # change MIP to LP 
+nrow <- length(get.rhs(model))
 
-cvec <- as.vector(model$objective)
-constraint <- model$constraints
-constraint_1 <- constraint[[1]]
-const_dir <- constraint[[2]]
-bvec <- constraint[[3]]
-bounds <- model$bounds
-types <- model$types
-maximum <- model$maximum
-nrow <- constraint_1$nrow 
-ncol <- constraint_1$ncol 
+# get A matrix
+model_Rglpk <- Rglpk_read_file(path, type = "MPS_fixed")
+Amat <- as.matrix(model_Rglpk$constraints[[1]])
+Amat <- cbind(diag(nrow), Amat)
 
-####################################
-#  read via Rglpk
-#  solve via linprog
-#  construct A matrix
-####################################
-Amat <- as.matrix(constraint_1)
-Amat <- rbind(Amat, diag(ncol)) # add upper bound into A
-bvec <- c(bvec, bounds$upper$val)
-const_dir <- c(const_dir, rep("<=", ncol))
-res <- solveLP(cvec, bvec, Amat, maximum = FALSE, const_dir)
+# get cvec, bvec
+cvec <- model_Rglpk$objective
+bvec <- model_Rglpk$constraints[[3]]
+dir <- as.vector(model_Rglpk$constraints[[2]])
 
 ####################################
 #  solving training LPs
 #  time consuming work
 ####################################
 first = TRUE
+beg_t <- proc.time()
 if (first) {
-  numtree <- 100
-  #reg <- array(NA, c(numtree, ncol*2, ncol)) too large
+  numtree <- 300
   vs_train <- matrix(NA, ncol = 2*ncol)
   sols <- matrix(NA, ncol = ncol)
+  Basis <- matrix(NA, ncol = nrow)
   beg_t <- proc.time()
   for (i in 1:numtree) {
-    newobjs <- similar_obj(objective)
-    newcons <- similar_cons(constraint_3)
-    res <- Rglpk_solve_LP(obj = newobjs, mat = constraint_1, dir = constraint_2, rhs = newcons, 
-                          bounds = rep("C",nrow), max = maximum)  
-    v <- c(v_train(res), as.vector(newobjs))
-    sol <- res$solution
+    newobjs <- as.vector(similar_obj(cvec))
+    newcons <- as.vector(similar_cons(bvec))
+    ## solve LP and generate A_b
+    set.objfn(model, obj = newobjs)
+    set.rhs(model, b = newcons)
+    solve(model)
+    B_loc <- get.basis(model)
+    Basis <- rbind(Basis, B_loc)  
+    sol <- get.variables(model)
+    v <- c(sol, newobjs)
     vs_train <- rbind(vs_train, v)
     sols <- rbind(sols, sol)
     cat(i)
@@ -68,9 +65,12 @@ if (first) {
   exc_t <- proc.time() - beg_t
   vs_train <- na.omit(vs_train)
   sols <- na.omit(sols)
+  Basis <- na.omit(Basis)
+  exc_t <- proc.time() - beg_t
   write.csv(sols, file = "./example/temp/train_sols.csv")
   write.csv(vs_train, file = "./example/temp/train_vector.csv")
-  rm(numtree, beg_t, newobjs, newcons, res, v, sol, i)
+  write.csv(Basis, file = "./example/temp/basis.csv")
+  rm(numtree, beg_t, newobjs, newcons, v, sol, i)
 } else {
   sols <- read.csv(file = "./example/temp/train_sols.csv", header = T)  
   vs_train <- read.csv(file = "./example/temp/train_vector.csv", header = T)  
@@ -79,44 +79,32 @@ if (first) {
 }
 
 ####################################
-#   training process
-####################################
-# ncol prediction models, time consuming, 0.5 seconds per prediction
-# model_list <- list()
-# for (i in 1:ncol) {
-#   reg <- cbind(vs, sols[,i])
-#   reg <- as.data.frame(reg)
-#   names(reg)[ncol(reg)] <- "y"
-#   model <- TrainModel(reg, "gbm")  #  Error: protect(): protection stack overflow 
-#   #model <- TrainModel(reg, "linear")
-#   #model <- TrainModel(as.matrix(reg), "xgboost")
-#   model_list[[i]] <- model
-# }
-
-####################################
 #   testing process
 ####################################
 # test solver
 num = 100
 vs_test <- matrix(NA, ncol = 2*ncol)
+vs_pred <- matrix(NA, ncol = 2*ncol)
 beg_t <- proc.time()
 while(num > 0) {
-  testobj <- similar_obj(objective) 
-  testcon <- similar_cons(constraint_3)
+  testobj <- as.vector(similar_obj(cvec))
+  testcon <- as.vector(similar_cons(bvec))
 
   # solution from LP solver
-  sol_solver <- Rglpk_solve_LP(obj = testobj, mat = constraint_1, dir = constraint_2, rhs = testcon, 
-                               bounds = rep("C",nrow), max = maximum)
+  set.objfn(model, obj = testobj)
+  set.rhs(model, b = testcon)
+  solve(model)
+  
+  vs_test <- rbind(vs_test, c(get.variables(model), testobj))
   
   # construct test vs
-  useSol = FALSE
-  if (useSol) {
-    vs_test <- rbind(vs_test, c(sol_solver$sol, as.vector(testobj)))
-  } else {
-    d <- predictor_vector(matrix(0, nrow = nrow, ncol = ncol), as.matrix(constraint_1), constraint_3, sols)
-    vs_test <- rbind(vs_test, c(d, as.vector(testobj)))
-  }
-  
+  B_loc <- abs(Basis[sample(300, 1),])
+  B <- Amat[, B_loc]
+  d_B <- round(solve(B) %*% testcon, 2)
+  vs <- rep(0, nrow + ncol)
+  vs[B_loc] <- d_B
+  vs_pred <- rbind(vs_pred, c(vs[-c(1:nrow)], testobj))
+
   num <- num - 1
   cat(num)
 }
@@ -139,8 +127,8 @@ sol <- matrix(NA, ncol = ncol)
 for (i in 1:num) {
   diff <- sweep(vs_train, 2, vs_test[i,])
   distance <- sqrt(rowSums(diff^2))
-  loc <- which(distance %in% sort(distance)[1:3])[1:3]
-  sol <- rbind(sol, colSums(vs_train[loc,1:ncol])/3)
+  loc <- which(distance %in% sort(distance)[1:5])[1:5]
+  sol <- rbind(sol, colSums(vs_train[loc,1:ncol])/5)
 }
 sol <- na.omit(sol)
 exc_t_prediction <- proc.time() - beg_t
@@ -155,10 +143,20 @@ C <- as.matrix(vs_test[,-c(1:ncol)])
 obj_solver <- rowSums(sol_solver * C)
 obj_prediction <- rowSums(sol_prediction * C)
 error <- round((obj_prediction-obj_solver)/obj_solver,2)
-hist(error)
+hist(error, main = "Objective Value Error of 5-NN in LP problem")
 
+# compare MIP
+BP_sol <- read.table(file = "./example/Data/Sample/air05.sol")
+BP_sol <- BP_sol[,2]
+BP_sol_pred <- round(sol_prediction)
+BP_obj <- rowSums(BP_sol * C)
+BP_obj_pred <- rowSums(BP_sol_pred * C)
+error <- round((BP_obj_pred-BP_obj)/BP_obj,2)
+hist(error, main = "Objective Value Error of 5-NN in BP problem")
+
+# compare correlation
 cor <- c()
 for (i in 1:num) {
   cor <- c(cor, cor(sol_prediction[i,], sol_solver[i,]))  
 }
-plot(cor)
+plot(cor, main = "Solution Correlation Between Solver and Prediction")
--- example/example3.R
+++ example/example3.R
@@ -11,23 +11,26 @@
 library("lpSolveAPI")
 library("Rglpk")
 library("linprog")
+library("clpAPI")
 
 ######################################################
 #  air05 example
 ######################################################
 rm(list=ls())
-#path <- paste(getwd(), "/example/Data/Sample/air05", sep = "")
-path <- paste(getwd(), "/example/Data/Sample/p0033.mps", sep = "")
+path <- paste(getwd(), "/example/Data/Sample/air05", sep = "")
+#path <- paste(getwd(), "/example/Data/Sample/p0033.mps", sep = "")
 lp <- read.lp(path, type = "mps")
 #write.lp(lp, filename = "./example/Data/Sample/p0033.lp", type = "lp")
 
 # change MIP to LP, and solve
 n <- length(get.type(lp))
+set.bounds(lp, upper = rep(Inf, n), columns = 1:n)
 set.type(lp, columns = 1:n, type = "real")
 solve(lp)
 m <- length(get.basis(lp))
 B_loc <- abs(get.basis(lp))
 res_lpSolveAPI <- get.variables(lp)
+res_lpSolveAPI[res_lpSolveAPI!=0]
 
 # read constraint matrix via Rglpk package
 model <- Rglpk_read_file(path, type = "MPS_fixed")
@@ -38,7 +41,10 @@ constraint <- cbind(diag(m), constraint)
 B <- constraint[,B_loc]
 rhs <- get.rhs(lp)
 d_B <- round(solve(B) %*% rhs,2)
+B_loc <- sort(B_loc)
 d_B[B_loc > m]
+
+
 
 ######################################################
 #   standard example
@@ -98,3 +104,37 @@ basis_slack <- basis[grep("S.*", basis)]; basis_slack <- as.numeric(substring(ba
 A_b <- Amat[,c(basis_slack, basis_var+49)]
 d_b <- round(solve(A_b) %*% bvec, 2)
 d_b_solved <- res_linprog$basvar
+
+
+#####################################################
+#####################################################
+rm(list=ls())
+# preparing the model
+lp <- initProbCLP()
+nrows <- 5
+ncols <- 8
+# objective function
+obj <- c(1, 0, 0, 0, 2, 0, 0, -1)
+# upper and lower bounds of the rows
+rlower <- c(2.5, -1000, 4, 1.8, 3)
+rupper <- c(1000, 2.1, 4, 5, 15)
+# upper and lower bounds of the columns
+clower <- c(2.5, 0, 0, 0, 0.5, 0, 0, 0)
+cupper <- c(1000, 4.1, 1, 1, 4, 1000, 1000, 4.3)
+# constraint matrix
+ia <- c(0, 4, 0, 1, 1, 2, 0, 3, 0, 4, 2, 3, 0, 4)
+ja <- c(0, 2, 4, 6, 8, 10, 11, 12, 14)
+ar <- c(3.0, 5.6, 1.0, 2.0, 1.1, 1.0, -2.0, 2.8,
+        -1.0, 1.0, 1.0, -1.2, -1.0, 1.9)
+# direction of optimization
+setObjDirCLP(lp, 1)
+# load problem data
+loadProblemCLP(lp, ncols, nrows, ia, ja, ar,
+               clower, cupper, obj, rlower, rupper)
+# solve lp problem
+solveInitialCLP(lp)
+# retrieve the results
+getSolStatusCLP(lp)
+getObjValCLP(lp)
+getColPrimCLP(lp)
+getRowPrimCLP(lp)
