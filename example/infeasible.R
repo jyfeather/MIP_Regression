81c3fea9257ef1a165478c3359dc7941574a6935
Yan Jin <jyfeather@gmail.com>
2015-06-07 20:32:23 -0700

    add infeasible example

---
+++ example/infeasible.R
@@ -0,0 +1,41 @@
+############################################
+# wonder the difference when facing an infeasible LP
+# http://lpsolve.sourceforge.net/5.5/
+############################################
+
+library(lpSolveAPI)
+
+############################################
+# simple problem
+############################################
+# w/o trick
+lprec <- make.lp(0, 2)
+set.objfn(lprec, c(1, 1))
+add.constraint(lprec, c(1, 0), ">=", 6)
+add.constraint(lprec, c(0, 1), ">=", 6)
+add.constraint(lprec, c(1, 1), "<=", 11)
+beg <- proc.time()
+n <- 10000
+while(n > 0) {
+  solve(lprec)
+  n <- n - 1
+}
+proc.time() - beg
+
+# w/ trick
+lprec <- make.lp(0, 5)
+set.objfn(lprec, c(1, 1, 1000, 1000, 1000))
+add.constraint(lprec, c(1, 0, 1, 0, 0), ">=", 6)
+add.constraint(lprec, c(0, 1, 0, 1, 0), ">=", 6)
+add.constraint(lprec, c(1, 1, 0, 0, -1), "<=", 11)
+beg <- proc.time()
+n <- 10000
+while(n > 0) {
+  solve(lprec)
+  n <- n - 1
+}
+proc.time() - beg
+
+###########################################
+# complex problem
+###########################################
